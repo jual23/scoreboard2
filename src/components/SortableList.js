b9cb85f6199d1b2395fa0559b204c0af223d067c
Juan Almillategui <juanalmillategui@grupodavid.com>
2022-07-05 17:36:19 -0500

    update css

--- src/components/SortableList.js
+++ src/components/SortableList.js
@@ -1,6 +1,8 @@
 import {DragDropContext} from 'react-beautiful-dnd'
 import {Draggable} from 'react-beautiful-dnd'
 import {Droppable} from 'react-beautiful-dnd'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import Stats from './Stats'
 
 const SortableList = ({
@@ -18,73 +20,80 @@ const SortableList = ({
             <div className="sortable-list_team">
                 <h2>{teamName}</h2>
             </div>
+            
             <DragDropContext onDragEnd={updateTeam}>
+            <Grid container spacing={2}>
                 <Droppable droppableId={teamId + '_batter'}>
                     {(provided, snapshot) => (
-                        <div
+                        <Grid container rowSpacing={2}
                             ref={provided.innerRef}
                             className="sortable-list_team_batter">
                             {teamBatter.map((player, index) => (
-                                <Draggable
-                                    draggableId={player.id}
-                                    key={player.id}
-                                    index={index}>
-                                    {(provided, snapshot) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}>
-                                            <p {...provided.dragHandleProps}>
-                                                DRAG
-                                            </p>
-                                            <Stats
-                                                key={player.id}
-                                                player={player}
-                                                onHandlePlayer={onHandlePlayer}
-                                                // statUp={statUp}
-                                                // statDown={statDown}
-                                                teamName={teamName}
-                                            />
-                                        </div>
-                                    )}
-                                </Draggable>
+                                <Grid container rowSpacing={2}>
+                                    <Draggable
+                                        draggableId={player.id}
+                                        key={player.id}
+                                        index={index}>
+                                        {(provided, snapshot) => (
+                                            <Grid container xs={12}
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}>
+                                                <Grid item xs={2} {...provided.dragHandleProps}>
+                                                    DRAG
+                                                </Grid>
+                                                <Grid item xs={10}>
+                                                    <Stats
+                                                    key={player.id}
+                                                    player={player}
+                                                    onHandlePlayer={onHandlePlayer}
+                                                    teamName={teamName}
+                                                    />
+                                                </Grid>
+                                            </Grid>
+                                        )}
+                                    </Draggable>
+                                </Grid>
                             ))}
                             {provided.placeholder}
-                        </div>
+                        </Grid>
                     )}
                 </Droppable>
                 <Droppable droppableId={teamId + '_reserve'}>
                     {(provided, snapshot) => (
-                        <div
+                        <Grid container rowSpacing={2}
                             ref={provided.innerRef}
                             className="sortable-list_team_reserve">
                             {team.map((player, index) => (
-                                <Draggable
-                                    draggableId={player.id}
-                                    key={player.id}
-                                    index={index}>
-                                    {(provided, snapshot) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}>
-                                            <p {...provided.dragHandleProps}>
-                                                DRAG
-                                            </p>
-                                            <Stats
-                                                key={player.id}
-                                                player={player}
-                                                onHandlePlayer={onHandlePlayer}
-                                                // statUp={statUp}
-                                                // statDown={statDown}
-                                                teamName={teamName}
-                                            />
-                                        </div>
-                                    )}
-                                </Draggable>
+                                <Grid item xs={12}>
+                                    <Draggable
+                                        draggableId={player.id}
+                                        key={player.id}
+                                        index={index}>
+                                        {(provided, snapshot) => (
+                                            <Grid container 
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}>
+                                                <Grid item xs={2} {...provided.dragHandleProps}>
+                                                    DRAG
+                                                </Grid>
+                                                <Grid item xs={10}>
+                                                    <Stats
+                                                    key={player.id}
+                                                    player={player}
+                                                    onHandlePlayer={onHandlePlayer}
+                                                    teamName={teamName}
+                                                    />
+                                                </Grid>
+                                            </Grid>
+                                        )}
+                                    </Draggable>
+                            </Grid>
                             ))}
                             {provided.placeholder}
-                        </div>
+                        </Grid>
                     )}
                 </Droppable>
+                </Grid>
             </DragDropContext>
         </div>
     )
