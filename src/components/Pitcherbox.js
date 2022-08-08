cf168497995a91e929f77319e92c6325b85b7d31
jual23 <almillastegui@hotmail.com>
2022-08-08 18:18:00 -0500

    update with harpia 2.0

--- src/components/Pitcherbox.js
+++ src/components/Pitcherbox.js
@@ -1,18 +1,15 @@
-import {
-    Autocomplete,
-    TextField,
-    Stack,
-    Grid,
-    Typography,
-    Box,
-    Button,
-    Card,
-} from '@mui/material'
+import {Autocomplete, TextField, Grid, Typography} from '@mui/material'
 import {useState} from 'react'
 import IconButton from '@mui/material/IconButton'
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow'
 
-const Pitcherbox = ({awayTeam, homeTeam, currentPitcher, onHandlePitcher,matchData}) => {
+const Pitcherbox = ({
+    awayTeam,
+    homeTeam,
+    currentPitcher,
+    onHandlePitcher,
+    matchData,
+}) => {
     const [valueD, setValueD] = useState()
     const [inputValueD, setInputValueD] = useState()
 
@@ -28,57 +25,66 @@ const Pitcherbox = ({awayTeam, homeTeam, currentPitcher, onHandlePitcher,matchDa
             {matchData.bottomHalf === false ? (
                 <Grid container>
                     <Grid item xs={12}>
-                    <Autocomplete
-                        value={valueD}
-                        onChange={(event, newValue) => {
-                            setValueD(newValue)
-                        }}
-                        inputValue={inputValueD}
-                        onInputChange={(event, newInputValue) => {
-                            setInputValueD(newInputValue)
-                        }}
-                        isOptionEqualToValue={(option, value) => option === value}
-                        options={homeTeam}
-                        getOptionLabel={option => option.number}
-                        renderInput={params => (
-                            <TextField {...params} label="Pitcher" />
-                        )}
+                        <Autocomplete
+                            value={valueD}
+                            onChange={(event, newValue) => {
+                                setValueD(newValue)
+                            }}
+                            inputValue={inputValueD}
+                            onInputChange={(event, newInputValue) => {
+                                setInputValueD(newInputValue)
+                            }}
+                            isOptionEqualToValue={(option, value) =>
+                                option === value
+                            }
+                            options={homeTeam}
+                            getOptionLabel={option => option.number}
+                            renderInput={params => (
+                                <TextField {...params} label="Pitcher" />
+                            )}
                         />
                     </Grid>
                     <Grid item xs={6}>
                         <IconButton onClick={() => onHandlePitcher(valueD)}>
-                            <DoubleArrowIcon color="primary" sx={{fontSize: 40}} />
+                            <DoubleArrowIcon
+                                color="primary"
+                                sx={{fontSize: 40}}
+                            />
                         </IconButton>
                     </Grid>
-                </Grid>)
-        :   (
-            <Grid container>
-            <Grid item xs={12}>
-            <Autocomplete
-                value={valueD}
-                onChange={(event, newValue) => {
-                    setValueD(newValue)
-                }}
-                inputValue={inputValueD}
-                onInputChange={(event, newInputValue) => {
-                    setInputValueD(newInputValue)
-                }}
-                isOptionEqualToValue={(option, value) => option === value}
-                options={awayTeam}
-                getOptionLabel={option => option.number}
-                renderInput={params => (
-                    <TextField {...params} label="Pitcher" />
-                )}
-                />
-            </Grid>
-            <Grid item xs={6}>
-                <IconButton onClick={() => onHandlePitcher(valueD)}>
-                    <DoubleArrowIcon color="primary" sx={{fontSize: 40}} />
-                </IconButton>
-            </Grid>
-        </Grid>)        
-        }
-            
+                </Grid>
+            ) : (
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Autocomplete
+                            value={valueD}
+                            onChange={(event, newValue) => {
+                                setValueD(newValue)
+                            }}
+                            inputValue={inputValueD}
+                            onInputChange={(event, newInputValue) => {
+                                setInputValueD(newInputValue)
+                            }}
+                            isOptionEqualToValue={(option, value) =>
+                                option === value
+                            }
+                            options={awayTeam}
+                            getOptionLabel={option => option.number}
+                            renderInput={params => (
+                                <TextField {...params} label="Pitcher" />
+                            )}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <IconButton onClick={() => onHandlePitcher(valueD)}>
+                            <DoubleArrowIcon
+                                color="primary"
+                                sx={{fontSize: 40}}
+                            />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            )}
         </div>
     )
 }
